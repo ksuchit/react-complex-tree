ab88294c6b3089cce36649af878fb1cf33bc634e
ksuchit <suchitkore1528@gmail.com>
2023-05-17 16:06:28 +0530

    first commit

---
+++ src/components/ReactComplexTree.js
@@ -0,0 +1,56 @@
+import { StaticTreeDataProvider, Tree, UncontrolledTreeEnvironment } from "react-complex-tree";
+import { items } from "./dataProvider";
+import { useState } from "react";
+
+export default function ReactComplexTree() {
+  
+    const [focusedItem, setFocusedItem] = useState();
+    const [expandedItems, setExpandedItems] = useState([]);
+    const [selectedItems, setSelectedItems] = useState([]);
+
+    const dataProvider = new StaticTreeDataProvider(items, (item, newName) => ({ ...item, data: newName }))
+    
+    console.log('focusedItem',focusedItem)
+    console.log('expandedItems',expandedItems)
+    console.log('selectedItems',selectedItems)
+    return (
+        <>
+            <style>{`
+        :root {
+          --rct-color-tree-bg: #F6F8FA;
+          --rct-color-focustree-item-selected-bg: #e2d3d3;
+          --rct-bar-color: none;
+          --rct-color-focustree-item-focused-border: #d60303;
+          --rct-color-focustree-item-draggingover-bg: #ecdede;
+          --rct-color-focustree-item-draggingover-color: inherit;
+          --rct-color-search-highlight-bg: #7821e2;
+          --rct-color-drag-between-line-bg: #cf03d6;
+          --rct-color-arrow: #b48689;
+          --rct-item-height: 30px;
+          --rct-arrow-size: 12px;
+          --rct-arrow-container-size: 18px;
+          --rct-color-focustree-item-hover-bg: #f0e4e4;
+        }
+      `}</style>
+        <UncontrolledTreeEnvironment
+        dataProvider={dataProvider}
+        getItemTitle={item => item.data}
+        viewState={{}}
+        canDragAndDrop={true}
+        canDropOnFolder={true}
+        canReorderItems={true}
+            
+        onFocusItem={item => setFocusedItem(item.index)}
+        onExpandItem={item => setExpandedItems([...expandedItems, item.index])}
+        onCollapseItem={item =>
+            setExpandedItems(expandedItems.filter(expandedItemIndex => expandedItemIndex !== item.index))
+        }
+        onSelectItems={items => setSelectedItems(items)}
+            
+      >
+        <Tree treeId="tree-1" rootItem="root" treeLabel="Tree Example" />
+        {/* <Tree treeId="tree-2" rootItem="root" treeLabel="Tree Example" /> */}
+         </UncontrolledTreeEnvironment>
+        </>
+  )
+}
---
+++ src/components/dataProvider.js
@@ -0,0 +1,235 @@
+
+export const items = {
+    root: {
+        index: "root",
+        isFolder: true,
+        children: ["Fruit","Meals" ,"Desserts", "Drinks"],
+        data: "Root item",
+      },
+      Fruit: {
+        index: "Fruit",
+        isFolder:true,  
+        children: ['Apple','Orange','Lemon','Berries','Banana'],
+        data: "Fruit",
+      },
+      Apple: {
+          index: "Apple",
+          children: [],
+          data: "Apple",
+      },
+      Orange: {
+          index: "Orange",
+          children: [],
+          data: "Orange",
+      },
+      Lemon: {
+          index: "Lemon",
+          children: [],
+          data: "Lemon",
+      },
+      Berries: {
+          index: "Berries",
+          isFolder:true,
+          children: ['Red','Blue','Black'],
+          data: "Berries",
+      },
+      Red: {
+          index: "Red",
+          isFolder:true,
+          children: ['Strawberry','Raspberry'],
+          data: "Red",
+      },
+      Strawberry: {
+          index: "Strawberry",
+          children: [],
+          data: "Strawberry",
+      },
+      Raspberry: {
+          index: "Raspberry",
+          children: [],
+          data: "Raspberry",
+      },
+      Blue: {
+          index: "Blue",
+          isFolder:true,
+          children: ['Blueberry'],
+          data: "Blue",
+      },
+      Blueberry: {
+          index: "Blueberry",
+          children: [],
+          data: "Blueberry",
+      },
+      Black: {
+          index: "Black",
+          isFolder:true,
+          children: ['Blackberry'],
+          data: "Black",
+      },
+      Blackberry: {
+          index: "Blackberry",
+          children: [],
+          data: "Blackberry",
+      },
+      Banana: {
+          index: "Banana",
+          children: [],
+          data: "Banana",
+      },
+
+      Meals: {
+        index: "Meals",
+        isFolder: true,
+        children:['America','Europe','Asia','Australia'],
+        data: "Meals",
+      },
+      America: {
+        index: "America",
+        isFolder:true,
+        children:['SmashBurger','Chowder','Ravioli','MacAndCheese','Brownies'],
+        data: "America",
+      },
+      SmashBurger: {
+        index: "SmashBurger",
+        children:[],
+        data: "SmashBurger",
+      },
+      Chowder: {
+        index: "Chowder",
+        children:[],
+        data: "Chowder",
+      },
+      Ravioli: {
+        index: "Ravioli",
+        children:[],
+        data: "Ravioli",
+      },
+      MacAndCheese: {
+        index: "MacAndCheese",
+        children:[],
+        data: "MacAndCheese",
+      },
+      Brownies: {
+        index: "Brownies",
+        children:[],
+        data: "Brownies",
+      },
+      Europe: {
+        index: "Europe",
+        isFolder:true,
+        children:['Risotto','Spaghetti','Pizza','Weisswurst','Spargel'],
+        data: "Europe",
+      },
+      Risotto: {
+        index: "Risotto",
+        children:[],
+        data: "Risotto",
+      },
+      Spaghetti: {
+        index: "Spaghetti",
+        children:[],
+        data: "Spaghetti",
+      },
+      Pizza: {
+        index: "Pizza",
+        children:[],
+        data: "Pizza",
+      },
+      Weisswurst: {
+        index: "Weisswurst",
+        children:[],
+        data: "Weisswurst",
+      },
+      Spargel: {
+        index: "Spargel",
+        children:[],
+        data: "Spargel",
+      },
+      Asia: {
+        index: "Asia",
+        isFolder:true,
+        children:['Curry','PadThai','Jiaozi','Jiaozi'],
+        data: "Asia",
+      },
+      Curry: {
+        index: "Curry",
+        children:[],
+        data: "Curry",
+      },
+      PadThai: {
+        index: "PadThai",
+        children:[],
+        data: "PadThai",
+      },
+      Jiaozi: {
+        index: "Jiaozi",
+        children:[],
+        data: "Jiaozi",
+      },
+      Sushi: {
+        index: "Sushi",
+        children:[],
+        data: "Sushi",
+      },
+      Australia: {
+        index: "Australia",
+        isFolder:true,
+        children:['Potato','PokeBowl','LemonCurd','KumaraFries'],
+        data: "Australia",
+      },
+      Potato: {
+        index: "Potato",
+        children:[],
+        data: "Potato",
+      },
+      PokeBowl: {
+        index: "PokeBowl",
+        children:[],
+        data: "PokeBowl",
+      },
+      LemonCurd: {
+        index: "LemonCurd",
+        children:[],
+        data: "LemonCurd",
+      },
+      KumaraFries: {
+        index: "KumaraFries",
+        children:[],
+        data: "KumaraFries",
+      },
+      
+      Desserts: {
+        index: "Desserts",
+        isFolder: true,
+        children: ["Cookies",'IceCream'],
+        data: "Desserts",
+      },
+      Cookies: {
+        index: "Cookies",
+        children: [],
+        data: "Cookies",
+      },
+       IceCream: {
+        index: "IceCream",
+        children: [],
+        data: "IceCream",
+      },
+       
+      Drinks: {
+        index: "Drinks",
+        isFolder: true,
+        children: ["Cola",'Juice'],
+        data: "Drinks",
+      },
+      Cola: {
+        index: "Cola",
+        children: [],
+        data: "Cola",
+      },
+       Juice: {
+        index: "Juice",
+        children: [],
+        data: "Juice",
+      },
+     
+  };
